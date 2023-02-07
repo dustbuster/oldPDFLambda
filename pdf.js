dcef8d5bf32eb02497417b1dd4229bc1cd31cde4
dustbuster <me@dustinhorning.net>
2023-02-07 16:40:50 -0500

    DEV-PUPPET: the start of my repo, adding files tearing apart to make work`

--- pdf.js
+++ pdf.js
@@ -6,7 +6,68 @@ const path = require('path')
 
 const knex = require('./src/db')
 
-module.exports.pdf = async (event, context) => {
+module.exports.generate_pdf_post_html = async (event) => {
+  
+  console.log('generate_pdf_post_html');
+
+  if (typeof event['body'] === 'string') {
+    var decodedText = convertBase64RequestToString(event['body']);
+    console.log('decodedText');
+    console.log(decodedText);
+  }
+
+  const template = pug.compileFile(decodedText)
+  console.log('template');
+  console.log(template);
+  // const html = template({result})
+
+  let browser = null
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    })
+
+    const page = await browser.newPage()
+    page.setContent(html)
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      displayHeaderFooter: true,
+      margin: { top: '1.8cm', right: '1cm', bottom: '1cm', left: '1cm' },
+    })
+
+    // TODO: Response with PDF (or error if something went wrong )
+    const response = {
+      headers: {
+        'Content-type': 'application/pdf',
+        'content-disposition': 'attachment; filename=test.pdf',
+      },
+      statusCode: 200,
+      body: pdf.toString('base64'),
+      isBase64Encoded: true,
+    }
+    context.succeed(response)
+  } catch (error) {
+    return context.fail(error)
+  } finally {
+    if (browser !== null) {
+      await browser.close()
+    }
+  }
+}
+
+function convertBase64RequestToString(base64Encoded) {
+  let buff = Buffer.from(base64Encoded, 'base64');  
+  let text = buff.toString('utf-8');
+  const pdfContent = decodeURI(text).replace('text=','');
+  return pdfContent;
+}
+
+
+module.exports.example = async (event, context) => {
   const yearMonth = ((event || {}).pathParameters || {}).yearMonth || ''
   const year = yearMonth.length == 7 && yearMonth.substring(0, 4)
   const month = yearMonth.length == 7 && yearMonth.substring(5, 6)
