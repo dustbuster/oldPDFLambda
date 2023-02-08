30625a98287b505313e2b5f00b93436f80dbdb46
dustbuster <me@dustinhorning.net>
2023-02-08 09:40:34 -0500

    DEV-6456: adding my updates to the repo

--- pdf.js
+++ pdf.js
@@ -1,44 +1,37 @@
 'use strict'
 const chromium = require('chrome-aws-lambda')
 const pug = require('pug')
-const fs = require('fs')
 const path = require('path')
 
-const knex = require('./src/db')
-
-module.exports.generate_pdf_post_html = async (event) => {
+module.exports.generate_pdf_post_html = async (event, context) => {
   
   console.log('generate_pdf_post_html');
-
+  
   if (typeof event['body'] === 'string') {
+    // console.log(event['body']);
     var decodedText = convertBase64RequestToString(event['body']);
     console.log('decodedText');
     console.log(decodedText);
   }
-
-  const template = pug.compileFile(decodedText)
-  console.log('template');
-  console.log(template);
-  // const html = template({result})
+  const html = decodedText;
 
   let browser = null
   try {
-    browser = await chromium.puppeteer.launch({
+      browser = await chromium.puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: chromium.headless,
     })
-
     const page = await browser.newPage()
     page.setContent(html)
     const pdf = await page.pdf({
       format: 'A4',
-      printBackground: true,
-      displayHeaderFooter: true,
-      margin: { top: '1.8cm', right: '1cm', bottom: '1cm', left: '1cm' },
+      // printBackground: true,
+      // displayHeaderFooter: true,
+      // margin: { top: '1.8cm', right: '1cm', bottom: '1cm', left: '1cm' },
     })
-
+    console.log(pdf)
     // TODO: Response with PDF (or error if something went wrong )
     const response = {
       headers: {
@@ -58,73 +51,24 @@ module.exports.generate_pdf_post_html = async (event) => {
     }
   }
 }
+
+// if (typeof event['body'] === 'string') {
+  // let data = event['body'];
+  // let buff = Buffer.from(data, 'base64');  
+  // let text = buff.toString('utf-8');
+//   const pdfContent = decodeURI(text).replace('text=','');
+//   console.log(pdfContent);
+//   console.log;opg
+// }
 
 function convertBase64RequestToString(base64Encoded) {
-  let buff = Buffer.from(base64Encoded, 'base64');  
+  let buff = Buffer.from(base64Encoded, 'base64');
+  // console.log('buff');
+  // console.log(buff);
   let text = buff.toString('utf-8');
-  const pdfContent = decodeURI(text).replace('text=','');
+  let pdfContent = decodeURI(text).replace('text=','');
+  pdfContent = pdfContent.replace('text=','');
+  console.log('pdfContent');
+  console.log(pdfContent);
   return pdfContent;
-}
-
-
-module.exports.example = async (event, context) => {
-  const yearMonth = ((event || {}).pathParameters || {}).yearMonth || ''
-  const year = yearMonth.length == 7 && yearMonth.substring(0, 4)
-  const month = yearMonth.length == 7 && yearMonth.substring(5, 6)
-
-  // Select a date
-  const selDate = new Date(year, month)
-  const filter = {
-    month: selDate.toLocaleString('en', { month: 'long' }),
-    year: selDate.getFullYear(),
-  }
-
-  // Fetch data with knex
-  const result = await knex
-    .select()
-    .from('sales')
-    .where({
-      year: filter.year,
-      month: selDate.getMonth() + 1,
-    })
-
-  const template = pug.compileFile('./src/template.pug')
-  const html = template({ ...filter, result })
-
-  let browser = null
-  try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-    })
-
-    const page = await browser.newPage()
-    page.setContent(html)
-    const pdf = await page.pdf({
-      format: 'A4',
-      printBackground: true,
-      displayHeaderFooter: true,
-      margin: { top: '1.8cm', right: '1cm', bottom: '1cm', left: '1cm' },
-    })
-
-    // TODO: Response with PDF (or error if something went wrong )
-    const response = {
-      headers: {
-        'Content-type': 'application/pdf',
-        'content-disposition': 'attachment; filename=test.pdf',
-      },
-      statusCode: 200,
-      body: pdf.toString('base64'),
-      isBase64Encoded: true,
-    }
-    context.succeed(response)
-  } catch (error) {
-    return context.fail(error)
-  } finally {
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
 }
